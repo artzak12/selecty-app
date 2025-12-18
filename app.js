36be2398b213e795de6406a6a81aef7eb798fe55
artzak12 <jaracrespoeduardo@gmail.com>
2025-12-18 11:57:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1135,4 +1135,4 @@ document.addEventListener('DOMContentLoaded', function() {
         filtroFecha.addEventListener('change', mostrarListaVentas);
     }
 });
-   Fix: Eliminar declaración duplicada de supabase
+
